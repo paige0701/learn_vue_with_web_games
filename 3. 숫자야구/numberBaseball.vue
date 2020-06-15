8fd831297a0845519d09356273aebd22b1da0660
Paige <paigechoi0701@gmail.com>
2020-06-15 21:35:26 +0900

    how to use for loop in vue

--- 3. 숫자야구/numberBaseball.vue
+++ 3. 숫자야구/numberBaseball.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
         <h1>{{result}}</h1>
-        <form v-on:submit="onSubmitForm">
+        <form @submit.prevent="onSubmitForm">
             <input ref="answer" maxlength="4" v-model="value">
-            <button>입략</button>
+            <button type="submit">입략</button>
         </form>
-        <div>시도: {{}}</div>
+        <div>시도: {{tries.length}}</div>
+        <ul>
+            <li v-for="t in tries">
+                <div>{{t.try}}</div>
+                <div>{{t.result}}</div>
+            </li>
+        </ul>
 
     </div>
 </template>
@@ -13,13 +19,17 @@
     export default {
         data() {
             return {
+                tries: [],
                 value: '',
                 result: ''
             }
         },
         methods: {
-          onSubmitForm(e) {
-              e.preventDefault()
+          onSubmitForm() {
+              // prevent default 대신 위에 @submit.prevent로 대체 가능
+              this.tries.push({try: this.value, result: '홈런'});
+              this.value = '';
+              this.$refs.answer.focus();
           }
         }
     }
